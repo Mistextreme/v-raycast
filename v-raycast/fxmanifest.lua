603149fc061fd7bf33cdbb1f3a42b68701585e86
Mist the Goat <62227356+Mistextreme@users.noreply.github.com>
2025-03-08 16:38:26 +0000

    Update fxmanifest.lua

--- v-raycast/fxmanifest.lua
+++ v-raycast/fxmanifest.lua
@@ -1,18 +1,18 @@
 fx_version 'cerulean'
 game 'gta5'
+lua54 'yes'
 
 author 'V-Scripts https://discord.gg/M9fXSG7wa8'
 description 'Get entity data from a raycast.'
 version '1.0.0'
 
-shared_script {
-    'config.lua',
-}
+shared_script '@ox_lib/init.lua'
+shared_script 'config.lua'
 
-client_script {
+client_scripts {
     'client/*.lua'
 }
 
-server_script {
+server_scripts {
     'server/*.lua'
 }
